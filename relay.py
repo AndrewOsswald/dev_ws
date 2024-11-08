006867c6c9fd4dbdb3565181336017fccaae0657
AndrewOsswald <142251658+AndrewOsswald@users.noreply.github.com>
2024-11-08 00:40:35 +0000

    add relay device

---
+++ relay.py
@@ -0,0 +1,17 @@
+from gpiozero import LED
+from time import sleep
+
+# Set up pins 9 and 10 as outputs
+pin_9 = LED(9)
+pin_10 = LED(10)
+
+# Turn on both pins
+pin_9.on()
+pin_10.on()
+
+# Keep the signal active for 5 seconds
+sleep(5)
+
+# Turn off both pins
+pin_9.off()
+pin_10.off()
